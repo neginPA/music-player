c5972e1252596e507ddea159ca5b4a92554eccfa
negin rezapanah <80negin.rezapanah@gmail.com>
2025-03-30 22:18:50 +0330

    changes are added

---
+++ javascript/script.js
@@ -0,0 +1,39 @@
+const audio = document.getElementById("audio");
+const time_past = document.getElementById("time-past");
+const time_left = document.getElementById("time-left");
+const fill = document.getElementById("fill");
+const fill_btn = document.getElementById("fill-btn");
+
+let played = false;
+
+function play_and_pause(){
+    if (played){
+        audio.pause();
+        document.getElementById("play-pause").src = "images/music-play-play-button-svgrepo-com (1).svg";
+        played = false;
+    }else{
+        audio.play();
+        document.getElementById("play-pause").src = "images/media-player-music-pause-svgrepo-com.svg";
+        played = true;
+    }
+
+}
+
+function change_format(time){
+    let minute = parseInt(time / 60);
+    let seconds = parseInt(time - (minute * 60));
+
+    if(seconds > 9){
+        return minute.toString() + ":" + seconds.toString();
+    }else{
+        return minute.toString() + ":0" + seconds.toString();
+    }
+}
+
+audio.addEventListener("timeupdate",function(){
+    fill.style.width = (audio.currentTime * 100 / audio.duration) + "%";
+    fill_btn.style.left = (audio.currentTime * 100 / audio.duration) + "%";
+    time_past.innerText = change_format(audio.currentTime);
+    time_left.innerText = "-" + change_format(audio.duration - audio.currentTime);
+
+});
